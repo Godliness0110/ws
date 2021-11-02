7c3ea3182578f62e149dda2d883ec2c4701083ff
godliness <godliness@hzw.com>
2021-11-02 14:40:46 +0800

    first commit

---
+++ web01/git-test/src/main/java/com/hzw/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.hzw.git;
+
+/**
+ * @author Godliness
+ * @date 2021/11/2 14:35
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
